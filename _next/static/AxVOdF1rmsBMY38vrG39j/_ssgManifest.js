484806e17e18de679b4901cb36ff716caa5dd65f
Tasnim Ankon <tasnim.ankon@gmail.com>
2025-02-03 00:55:25 +0000

    Updates

---
+++ _next/static/AxVOdF1rmsBMY38vrG39j/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F[categoryId]","\u002F[categoryId]\u002F[slug]","\u002F[categoryId]\u002Fpage\u002F[id]","\u002Fpage\u002F[id]","\u002Ftags\u002F[tagId]","\u002Ftags\u002F[tagId]\u002Fpage\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
